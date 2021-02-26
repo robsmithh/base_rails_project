02232bee716df8918ac1bdc9ec50a0e75efbb4ee
Rob <robsmithh@live.co.uk>
2021-02-26 15:48:25 +0000

    added password to database

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,17 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_222_151_915) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2021_02_26_143015) do
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email_address'
-    t.string 'first_name'
-    t.string 'last_name'
-    t.datetime 'dob'
-    t.string 'username'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "dob"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_hash"
   end
+
 end
